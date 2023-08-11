ff2a57e2bc2366f703a723d1a34e8dbfc23fceb3
Mert <mertqkaratekin@gmail.com>
2023-08-11 17:12:15 +0300

    Kullanıcının eksik bilgi ile kayıt olmasını engellemek için gerekli alanlar zorunlu hale getirildi.

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -1,9 +1,14 @@
-﻿namespace ControlCenterX.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlCenterX.Models
 {
     public class Candidate   //Model ile ilgili tanımlamalar yapıldı.
     {
+        [Required(ErrorMessage ="E-mail is required.")]  //Zorunlu alan
         public String? Email { get; set; } = String.Empty;  //Email alanı tanımlandığı anda = "" bu şekilde olacak
+        [Required(ErrorMessage = "FirstName is required.")]  //Zorunlu alan
         public String? FirstName { get; set; } = String.Empty;
+        [Required(ErrorMessage = "LastName is required.")]  //Zorunlu alan
         public String? LastName { get; set; } = String.Empty;
         public String? FullName => $"{FirstName} {LastName?.ToUpper()}"; //? var ise o işlemi yap.
         public int? Age { get; set; }
