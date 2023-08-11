35cfd40adaec1df9167d4aede652ae8ca48e7fb2
Mert <mertqkaratekin@gmail.com>
2023-08-11 08:48:40 +0300

    Yeni bir özellik ve yorum satırları eklendi.

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -2,15 +2,17 @@
 {
     public class Candidate   //Model ile ilgili tanımlamalar yapıldı.
     {
-        public String? Email { get; set; } = String.Empty;
+        public String? Email { get; set; } = String.Empty;  //Email alanı tanımlandığı anda = "" bu şekilde olacak
         public String? FirstName { get; set; } = String.Empty;
         public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName}{LastName?.ToUpper()}"; //? var ise o işlemi yap.
         public int? Age { get; set; }
         public String? SelectedCourse { get; set; } = String.Empty;
         public DateTime ApplyAt { get; set; }
         public Candidate()  //Default olarak o an ki tarih eklendi. 
         {
-            ApplyAt=DateTime.Now;
+            ApplyAt=DateTime.Now;  //ApplyAt nesnesi üretildiği anda default güncel tarih gelecek.
+            /*Eğer constructor tanımlamadan atama yapılsaydı üretildiği anda atanmazdı değer.*/
         }
 
     }
