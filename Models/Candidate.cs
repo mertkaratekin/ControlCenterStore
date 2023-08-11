5981e6f5818c6a5585875bd089ec75c0305b7785
Mert <mertqkaratekin@gmail.com>
2023-08-11 08:40:04 +0300

    Add project files.

---
+++ Models/Candidate.cs
@@ -0,0 +1,17 @@
+﻿namespace ControlCenterX.Models
+{
+    public class Candidate   //MODEL İLE İLGİLİ TANIMLAMALAR YAPILDI.
+    {
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+        public Candidate()  //Default olarak o an ki tarih eklendi. 
+        {
+            ApplyAt=DateTime.Now;
+        }
+
+    }
+}
