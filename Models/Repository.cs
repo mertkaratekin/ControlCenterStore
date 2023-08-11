dd47f94b3d33a8b3814428447c0b0421e0b718d5
Mert <mertqkaratekin@gmail.com>
2023-08-11 11:44:48 +0300

    Repository model oluşturuldu.

---
+++ Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlCenterX.Models
+{
+    public static class Repository
+    {
+        private static List<Candidate> applicaitons = new(); //Erişime kaptmak için private yapıldı.
+        public static IEnumerable<Candidate> Applications => applicaitons;
+        /*Sınıf içerisindeki listeye bakarak yanıt verecek.*/
+        /*Listeye sınıf dışarısından erişmek isteyenlere interface desteğiyle dönüş yapılıyor. */
+        
+        public static void Add(Candidate candidate) /*Formdan gelen elemanlar eklenecek*/
+        {
+            applicaitons.Add(candidate);/*private olarak tanımlanan listeye ekleme işlemi yapacak*/
+        }
+    }
+}
